74679e8154b5af7d49565a25c7b955c5799b2619
gthiffault <gabriel.thiffault@gmail.com>
2019-05-17 19:27:49 -0400

    Friday v14

--- functions.php
+++ functions.php
@@ -480,6 +480,15 @@ $urls = array_diff( $urls, array( $emoji_svg_url ) );
 return $urls;
 }
 
+// Remove jQuery Migrate Script from header and Load jQuery from Google API
+function crunchify_stop_loading_wp_embed_and_jquery() {
+	if (!is_admin()) {
+		wp_deregister_script('wp-embed');
+		wp_deregister_script('jquery');  // Bonus: remove jquery too if it's not required
+	}
+}
+add_action('init', 'crunchify_stop_loading_wp_embed_and_jquery');
+
 
 /*
  * -----------------------------------------------------------------------------
