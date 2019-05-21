c5e93bd6cd4e1b1a84b9aa987efb71e6675c1f87
gthiffault <gabriel.thiffault@gmail.com>
2019-05-21 08:49:40 -0400

    Thursday v6

--- functions.php
+++ functions.php
@@ -491,6 +491,13 @@ function crunchify_stop_loading_wp_embed_and_jquery() {
 }
 add_action('init', 'crunchify_stop_loading_wp_embed_and_jquery');
 
+function add_file_types_to_uploads($file_types){
+$new_filetypes = array();
+$new_filetypes['svg'] = 'image/svg+xml';
+$file_types = array_merge($file_types, $new_filetypes );
+return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
 
 /*
  * -----------------------------------------------------------------------------
