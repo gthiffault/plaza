2b8c16f991c994709f77afe6ffb58b1d0c55c118
gthiffault <gabriel.thiffault@gmail.com>
2019-05-21 01:15:13 -0400

    Thursday v4

--- footer.php
+++ footer.php
@@ -54,6 +54,6 @@
 	</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
-<script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/app.js' defer'></script>
+<script type='text/javascript' src='<?php echo get_template_directory_uri();?>/js/app.js' defer async'></script>
 </body>
 </html>
