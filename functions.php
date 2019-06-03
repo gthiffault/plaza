063f91d26e0ba6b66bd24179dbd004c6d760eec3
gthiffault <gabriel.thiffault@gmail.com>
2019-06-03 16:34:25 -0400

    Monday v18

--- functions.php
+++ functions.php
@@ -338,7 +338,7 @@ function populate_posts( $form ) {
         $choices = array();
  
         foreach ( $posts as $post ) {
-        	$post_language_information = wpml_get_language_information($post->id);
+        	$post_language_information = wpml_get_language_information(get_the_ID());
         	if($post_language_information['language_code'] == ICL_LANGUAGE_CODE) {
             	$choices[] = array( 'text' => $post->post_title, 'value' => $post->post_title );
         	}
